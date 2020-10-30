fd697a063b170006daba1c97ae4a950390159543
mesamesamesamesa <73683412+mesamesamesamesa@users.noreply.github.com>
2020-10-30 17:03:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>63070139</title>
+    </head>
+    <body>
+        <h1>ชื่อ:นายเมษา มานิตสกุลวงศ์ ชื่อเล่น:พี</h1>
+        <p>Cat and Dog</p>
+        <img src="cat.jpg">
+        <img src="dog.jpg">
+        <br>
+        <a href="https://www.facebook.com/">I like this</a>
+        
+        <h2>เรื่องตลก</h2>
+        <ul>
+            <li>ทดสอบ</li>
+            <li>ทดสอบ</li>
+            <li>ทดสอบ</li>
+        </ul>
+    </body>
+</html>
